2501f5efd8c1afcf0192a3a5dd66aeec779396ca
s10128109 <user.email>
2017-09-30 22:31:23 +0800

    clean blog主題套用完成

--- hello/controllers/default.go
+++ hello/controllers/default.go
@@ -9,7 +9,43 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "google.com"
-	c.Data["Email"] = "jcliang@cycu.org.tw"
+	//c.Data["Website"] = "classroom.google.com"
+	//c.Data["Email"] = "jcliang@cycu.org.tw"
+	//c.TplName = "index.tpl"
+	c.Layout = "layout.tpl"
+	c.LayoutSections = make(map[string]string)
+	c.LayoutSections["Header"] = "header.tpl"
+	c.LayoutSections["Footer"] = "footer.tpl"
 	c.TplName = "index.tpl"
+
+}
+
+func (c *MainController) About() {
+	//c.Data["Website"] = "classroom.google.com"
+	//c.Data["Email"] = "jcliang@cycu.org.tw"
+	//c.TplName = "about.tpl"
+	c.Layout = "layout.tpl"
+	c.LayoutSections = make(map[string]string)
+	c.LayoutSections["Header"] = "header.tpl"
+	c.LayoutSections["Footer"] = "footer.tpl"
+	c.TplName = "about.tpl"
+
+}
+
+func (c *MainController) SamplePost() {
+	c.Layout = "layout.tpl"
+	c.LayoutSections = make(map[string]string)
+	c.LayoutSections["Header"] = "header.tpl"
+	c.LayoutSections["Footer"] = "footer.tpl"
+	c.TplName = "samplePost.tpl"
+
+}
+
+func (c *MainController) Contact() {
+	c.Layout = "layout.tpl"
+	c.LayoutSections = make(map[string]string)
+	c.LayoutSections["Header"] = "header.tpl"
+	c.LayoutSections["Footer"] = "footer.tpl"
+	c.TplName = "contact.tpl"
+
 }
--- hello/routers/router.go
+++ hello/routers/router.go
@@ -2,9 +2,13 @@ package routers
 
 import (
 	"beegoWeb/hello/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/about", &controllers.MainController{}, "get,post:About")
+	beego.Router("/samplePost", &controllers.MainController{}, "get,post:SamplePost")
+	beego.Router("/contact", &controllers.MainController{}, "get,post:Contact")
 }
