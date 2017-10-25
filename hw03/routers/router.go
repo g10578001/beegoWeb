ea53d35639bd69647008ae9dd82bf4d437f8b15d
s10128109 <dachurita@gmail.com>
2017-10-25 08:23:10 +0800

    add signup form

--- hw03/routers/router.go
+++ hw03/routers/router.go
@@ -2,9 +2,11 @@ package routers
 
 import (
 	"beegoWeb/hw03/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/signup", &controllers.MainController{}, "get,post:Signup")
 }
